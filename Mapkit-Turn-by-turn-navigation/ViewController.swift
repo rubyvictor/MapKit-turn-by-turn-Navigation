baeaffb26d61338f251c1f7a296d17e2d5b7fb7c
victor lee ming yew <victor_li@flyscoot.com>
2019-05-09 22:35:20 +0800

    Initial Commit

---
+++ Mapkit-Turn-by-turn-navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mapkit-Turn-by-turn-navigation
+//
+//  Created by Victor Lee on 9/5/19.
+//  Copyright © 2019 VictorLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mapkit-Turn-by-turn-navigationTests/Mapkit_Turn_by_turn_navigationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Mapkit_Turn_by_turn_navigationTests.swift
+//  Mapkit-Turn-by-turn-navigationTests
+//
+//  Created by Victor Lee on 9/5/19.
+//  Copyright © 2019 VictorLee. All rights reserved.
+//
+
+import XCTest
+@testable import Mapkit_Turn_by_turn_navigation
+
+class Mapkit_Turn_by_turn_navigationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
