4de7d94e2bee20f6ce02b0d645cd2a0d81a1ab0e
victor lee ming yew <victor_li@flyscoot.com>
2019-05-09 23:16:29 +0800

    Setup Mapkit Project With Science centre GPX Location Coordinates

--- Mapkit-Turn-by-turn-navigation/ViewController.swift
+++ Mapkit-Turn-by-turn-navigation/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var directionsLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    //MARK: - Step three Store User Coordinates
+    var currentCoordinates: CLLocationCoordinate2D!
+    
+    fileprivate func setupLocationManager() {
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.startUpdatingLocation()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //MARK: - Step one setup
+        setupLocationManager()
     }
+}
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //MARK: - Step two get user's current location
+        manager.startUpdatingLocation()
+        guard let currentLocation = locations.first else { return }
+        //MARK: - Step four set current coordinate
+        currentCoordinates = currentLocation.coordinate
+        //MARK: - Step five set heading to track user direction
+        mapView.userTrackingMode = .followWithHeading
+        
+        
+    }
+}
 
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("")
+    }
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        return MKOverlayRenderer()
+    }
 }
 
